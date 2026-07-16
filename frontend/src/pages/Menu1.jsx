fe46ce1e1a2cb8e33f1d61da69f821d08635c1e8
skc4365 <50658047+skc4365@users.noreply.github.com>
2026-07-16 16:14:18 +0900

    수정 메뉴명

--- frontend/src/pages/Menu1.jsx
+++ frontend/src/pages/Menu1.jsx
@@ -4,6 +4,8 @@ export default function Menu1() {
   return (
     <div>
       menu-1
+      소개, 경력, 
+      정규과정,기업출강,온라인강의
     </div>
   )
 }
--- frontend/src/pages/SUB1_menu2.jsx
+++ frontend/src/pages/SUB1_menu2.jsx
@@ -4,6 +4,7 @@ export default function SUB1_menu2() {
   return (
     <div>
       menu-2-sub1
+      스프링부트4.1.0
     </div>
   )
 }
